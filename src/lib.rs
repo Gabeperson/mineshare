46b94ba68b3b3287e9ab4f3538cd82255c843929
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2025-05-28 17:09:48 -0700

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
-mod wordlist;
+pub mod wordlist;
+use std::num::NonZeroU32;
+use std::pin::Pin;
+use std::pin::pin;
+use std::sync::Arc;
+use std::task::Poll;
+
+use futures::StreamExt;
+use futures::pin_mut;
+use futures::stream::FuturesUnordered;
+use governor::RateLimiter;
+use governor::clock::DefaultClock;
+use governor::middleware::NoOpMiddleware;
+use governor::state::InMemoryState;
+use governor::state::NotKeyed;
 use tokio::io::AsyncRead;
 use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWrite;
 use tokio::io::AsyncWriteExt;
+use tokio::io::ReadBuf;
+use tokio::net::TcpStream;
 
-pub const DATA_BUF_SIZE: usize = 512 * 1024;
+pub const RECV_BUF_SIZE: usize = 256 * 1024;
+pub const TIMEOUT_DURATION_SECS: f32 = 15.;
 
 #[derive(Clone, Debug)]
 pub enum Message<'a> {
@@ -13,6 +30,7 @@ pub enum Message<'a> {
     Connect(u64),
     Data(&'a [u8]),
     HeartBeat,
+    Acknowledged(u64),
 }
 
 impl<'a> Message<'a> {
@@ -23,6 +41,7 @@ impl<'a> Message<'a> {
             Message::Connect(_) => 2,
             Message::Data(_) => 3,
             Message::HeartBeat => 4,
+            Message::Acknowledged(_) => 5,
         }
     }
     pub async fn encode<S: AsyncWrite + Unpin>(self, writer: &mut S) -> Result<(), std::io::Error> {
@@ -34,8 +53,8 @@ impl<'a> Message<'a> {
                 writer.write_all(d).await?;
                 Ok(())
             }
-            Message::Connect(id) | Message::Disconnect(id) => {
-                writer.write_u64(id).await?;
+            Message::Connect(n) | Message::Disconnect(n) | Message::Acknowledged(n) => {
+                writer.write_u64(n).await?;
                 Ok(())
             }
             Message::Data(bytes) => {
@@ -71,7 +90,7 @@ impl<'a> Message<'a> {
             }
             3 => {
                 let len = varint::decode_varint(reader).await? as usize;
-                if len >= DATA_BUF_SIZE {
+                if len >= RECV_BUF_SIZE {
                     return Err(std::io::ErrorKind::OutOfMemory.into());
                 }
                 let _len = reader.read_exact(&mut buf[..len as usize]).await?;
@@ -113,4 +132,166 @@ pub mod varint {
         }
         Err(std::io::ErrorKind::InvalidData.into())
     }
+}
+
+pub type Limiter = RateLimiter<NotKeyed, InMemoryState, DefaultClock, NoOpMiddleware>;
+
+#[derive(Debug)]
+pub struct SingleRecvConn {
+    pub stream: TcpStream,
+    pub limiter: Arc<Limiter>,
+    pub buf: Box<[u8; 8096]>,
+    pub buf_filled: Option<NonZeroU32>,
+    pub id: usize,
+}
+
+impl SingleRecvConn {
+    pub fn recv(&mut self) -> ConnReceiveDataFuture {
+        ConnReceiveDataFuture { client: Some(self) }
+    }
+}
+
+#[derive(Debug)]
+pub struct ConnReceiveDataFuture<'a> {
+    // Lifetime workaround
+    client: Option<&'a mut SingleRecvConn>,
+}
+
+impl<'a> Future for ConnReceiveDataFuture<'a> {
+    type Output = (Result<&'a [u8], std::io::Error>, usize);
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        let client = self.client.take().unwrap();
+        match client.buf_filled {
+            Some(n) => {
+                {
+                    let fut = client.limiter.until_n_ready(n);
+                    let fut = pin!(fut);
+                    match fut.poll(cx) {
+                        Poll::Ready(res) => {
+                            // Since we use 8KB buffer this should always succeed
+                            debug_assert!(res.is_ok());
+                        }
+                        Poll::Pending => return Poll::Pending,
+                    }
+                }
+                // Buffer is filled && we have enough quota to send this section
+                // We don't update buf_filled here because we don't know if the other side of the
+                // multiplexed stream has enough quota to receive
+                let n = n.get() as usize;
+                Poll::Ready((Ok(&client.buf[..n]), client.id))
+            }
+            None => {
+                let filled = {
+                    let stream = &mut client.stream;
+                    pin_mut!(stream);
+                    let mut buffer = ReadBuf::new(&mut *client.buf);
+                    match stream.poll_read(cx, &mut buffer) {
+                        Poll::Ready(v) => {
+                            if let Err(e) = v {
+                                // The TCP connection disconnected, bubble up to handle it
+                                return Poll::Ready((Err(e), client.id));
+                            }
+                            buffer.filled_mut().len()
+                        }
+                        // Stream doesn't have any extra data, we just keep polling
+                        Poll::Pending => return Poll::Pending,
+                    }
+                };
+                let filled = filled as u32; // We use an 8KB buffer so it's impossible for this to get above u32 limit.
+                if filled == 0 {
+                    // EOF, pass it on
+                    return Poll::Ready((Ok(&[]), client.id));
+                }
+                let filled = NonZeroU32::new(filled).unwrap();
+                let fut = client.limiter.until_n_ready(filled);
+                let fut = pin!(fut);
+                match fut.poll(cx) {
+                    Poll::Ready(res) => {
+                        // Since we use 8kb buffer this should always succeed
+                        debug_assert!(res.is_ok());
+                    }
+                    Poll::Pending => {
+                        // We received data, but don't have enough quota to send it.
+                        // So we update the buffer filled amount and wait until enough quota is filled
+                        client.buf_filled = Some(filled);
+                        return Poll::Pending;
+                    }
+                }
+                // We have data, and have enough quota to send it!
+                Poll::Ready((Ok(&client.buf[..filled.get() as usize]), client.id))
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PendingIfZero<Fut: Future> {
+    pub unordered: FuturesUnordered<Fut>,
+}
+
+impl<Fut: Future> PendingIfZero<Fut> {
+    pub fn new(unordered: FuturesUnordered<Fut>) -> Self {
+        Self { unordered }
+    }
+}
+
+impl<Fut: Future> Future for PendingIfZero<Fut> {
+    type Output = Fut::Output;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+        let Poll::Ready(opt) = self.unordered.poll_next_unpin(cx) else {
+            // futures unordered is still polling, so we are polling too
+            return Poll::Pending;
+        };
+        let Some(fut) = opt else {
+            // If futures unordered returns None, that means there's 0 futures inside it.
+            // So we should just return polling so the other "select" branches can run
+            return Poll::Pending;
+        };
+        Poll::Ready(fut)
+    }
+}
+
+#[derive(Debug)]
+pub struct RecvBuffer {
+    pub inner: [u8; RECV_BUF_SIZE],
+    pub start: usize,
+    // If usize::MAX, then empty
+    pub end: usize,
+}
+impl Default for RecvBuffer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl RecvBuffer {
+    pub fn new() -> Self {
+        Self {
+            inner: [0; RECV_BUF_SIZE],
+            start: 0,
+            end: usize::MAX,
+        }
+    }
+    pub fn len(&self) -> usize {
+        if self.end == usize::MAX {
+            return 0;
+        }
+        let len = (self.end + RECV_BUF_SIZE - self.start) % RECV_BUF_SIZE;
+        if len == 0 {
+            return RECV_BUF_SIZE;
+        }
+        len
+    }
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+    pub fn available(&self) -> usize {
+        RECV_BUF_SIZE - self.len()
+    }
+}
+
+pub struct MultiRecvConn {
+    limiter: Arc<Limiter>,
+    buf: RecvBuffer,
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,28 @@
-use std::collections::HashMap;
+use anyhow::Result;
+use futures::stream::FuturesUnordered;
+use futures::{FutureExt, pin_mut};
+use governor::RateLimiter;
+use governor::clock::DefaultClock;
+use governor::middleware::NoOpMiddleware;
+use governor::state::{InMemoryState, NotKeyed};
+use mineshare::*;
+use rand::Rng as _;
+use rand::seq::IndexedRandom;
+use std::collections::{HashMap, VecDeque};
+use std::net::SocketAddr;
+use std::num::NonZeroU32;
+use std::pin::{Pin, pin};
 use std::sync::Arc;
+use std::task::Poll;
+use std::time::Duration;
+use tokio::io::{AsyncRead as _, AsyncWriteExt as _, ReadBuf};
+use tokio::time::Instant;
 
 use clap::Parser;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::RwLock;
 use tokio::sync::mpsc::{Receiver, Sender, channel};
+
 #[tokio::main]
 async fn main() {
     async_main().await
@@ -12,9 +30,8 @@ async fn main() {
 
 async fn async_main() {
     let args = Args::parse();
-    let map = Arc::new(RwLock::new(
-        HashMap::<Vec<u8>, Sender<(TcpStream, Vec<u8>)>>::new(),
-    ));
+    let base_domain: Arc<str> = Arc::from(args.base_domain);
+    let map = Arc::new(RwLock::new(HashMap::<Vec<u8>, Sender<TcpStream>>::new()));
     let server_listener = match TcpListener::bind(&args.server_socket_addr).await {
         Ok(l) => l,
         Err(e) => {
@@ -35,13 +52,91 @@ async fn async_main() {
             std::process::exit(1);
         }
     };
+    let base_domain2 = base_domain.clone();
+    let map2 = map.clone();
+    tokio::task::spawn(async move {
+        let map = map2;
+        loop {
+            let (stream, addr) = server_listener
+                .accept()
+                .await
+                .expect("Accepting client shouldn't fail");
+            let map = map.clone();
+            let base_domain = base_domain2.clone();
+            tokio::task::spawn(async move {
+                let res = server_handler(stream, addr, map, base_domain).await;
+                if let Err(e) = res {
+                    eprintln!("Server handler returned error: {e}");
+                }
+            });
+        }
+    });
+}
+
+async fn server_handler(
+    mut stream: TcpStream,
+    addr: SocketAddr,
+    map: Arc<RwLock<HashMap<Vec<u8>, Sender<TcpStream>>>>,
+    base_domain: Arc<str>,
+) -> Result<()> {
+    let mut prefix = {
+        let locked = map.read().await;
+        get_random_prefix(&locked)
+    };
+    prefix.push(b'.');
+    prefix.extend(base_domain.as_bytes());
+    let url = prefix;
+    Message::DomainDecided(&url).encode(&mut stream).await?;
+
+    let mut clients: Vec<SingleRecvConn> = Vec::new();
+    let mut recv_buffer_limits: Vec<()> = Vec::new();
+    let mut timeout_time = Instant::now() + Duration::from_secs_f32(TIMEOUT_DURATION_SECS);
+
+    loop {
+        let client_fut = PendingIfZero::new(
+            clients
+                .iter_mut()
+                .map(|client| client.recv())
+                .collect::<FuturesUnordered<ConnReceiveDataFuture<'_>>>(),
+        );
+    }
+}
+
+// struct ReadClientVec<'a> {
+//     inner: &'a mut [Client],
+//     buf: ReadBuf<'a>,
+// }
+
+// impl<'a> Future for ReadClientVec<'a> {
+//     type Output = ();
+
+//     fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+//         for client in self.inner {
+//             let mut pin = Pin::new(&mut client.stream);
+//             match pin.poll_read(&mut self.buf, cx) {}
+//         }
+
+//         todo!()
+//     }
+// }
+
+fn get_random_prefix(map: &HashMap<Vec<u8>, Sender<TcpStream>>) -> Vec<u8> {
+    use wordlist::WORD_LIST;
+    let mut s = Vec::new();
+    while map.contains_key(&s) {
+        let mut rng = rand::rng();
+        let first = WORD_LIST.choose(&mut rng).unwrap();
+        let second = WORD_LIST.choose(&mut rng).unwrap();
+        s = format!("{first}-{second}").into_bytes();
+    }
+    s
 }
 
 #[derive(Parser, Debug)]
 struct Args {
     base_domain: String,
-    #[arg(default_value = "localhost:25565")]
+    #[arg(long, default_value = "localhost:25565")]
     client_socket_addr: String,
-    #[arg(default_value = "localhost:25564")]
+    #[arg(long, default_value = "localhost:25564")]
     server_socket_addr: String,
 }
