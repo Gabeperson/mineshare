f0f04db75e7cc76ddee832a0902d47f316ecc795
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2025-06-02 00:25:41 -0700

    Setup CI and change to tracing for client.rs

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 #![allow(clippy::cast_possible_truncation)]
 use clap::Parser;
-use std::{io::ErrorKind, net::SocketAddr, str::FromStr as _, time::Duration};
+use std::{io::ErrorKind, net::SocketAddr, str::FromStr as _, sync::Arc, time::Duration};
 use tokio::{
     io::{AsyncReadExt as _, AsyncWriteExt},
     net::{TcpStream, tcp::OwnedReadHalf},
@@ -17,20 +17,26 @@ async fn main() {
 }
 
 async fn async_main() {
+    tracing_subscriber::fmt::init();
     let args = Args::parse();
-    println!("Starting proxy connection");
-    let mut proxy_conn = match TcpStream::connect(&args.proxy_server).await {
+    info!("Starting proxy connection");
+    let mut proxy_conn = match TcpStream::connect(&format!(
+        "{}:{}",
+        args.proxy_server, args.proxy_server_connection_port
+    ))
+    .await
+    {
         Ok(l) => l,
         Err(e) => {
-            eprintln!(
+            error!(
                 "Failed to connect to proxy server `{}`: {e}",
                 args.proxy_server
             );
             std::process::exit(1);
         }
     };
-    println!("Proxy connection completed");
-    println!("Fetching Url");
+    info!("Proxy connection completed");
+    info!("Fetching Url");
     let mut u64_buf = [0u8; 8];
     let domain_fut = async {
         proxy_conn.read_exact(&mut u64_buf).await?;
@@ -50,17 +56,17 @@ async fn async_main() {
     let domain = match domain {
         Ok(d) => d,
         Err(e) => {
-            eprintln!("Failed to read domain from proxy sever: {e}");
+            error!("Failed to read domain from proxy sever: {e}");
             std::process::exit(1);
         }
     };
-    println!("Fetched Url");
-    println!("Proxy url: {domain}");
+    info!("Fetched Url");
+    info!("Proxy url: {domain}");
     let (recv, mut send) = proxy_conn.into_split();
     tokio::task::spawn(async move {
         loop {
             if let Err(e) = send.write_all(b"heartbeat").await {
-                eprintln!("Failed to heartbeat server: {e}");
+                error!("Failed to heartbeat server: {e}");
                 std::process::exit(1);
             }
             tokio::time::sleep(Duration::from_secs(5)).await;
@@ -72,51 +78,57 @@ async fn async_main() {
 }
 
 async fn main_loop(mut recv: OwnedReadHalf, args: Args) {
+    let proxy_play: Arc<str> = Arc::from(format!(
+        "{}:{}",
+        args.proxy_server, args.proxy_server_play_port
+    ));
     loop {
         let id = match recv.read_u128().await {
             Ok(id) => id,
             Err(e) => {
-                eprintln!("Server disconnected: {e}");
+                error!("Server disconnected: {e}");
                 std::process::exit(1);
             }
         };
-        println!("Server sent request with id: {id}");
-        let addr = args.proxy_server_play.clone();
+        info!("Server sent request with id: {id}");
+        // let addr = args.proxy_server_play.clone();
+        let proxy_play = proxy_play.clone();
         let saddr = args.server_socket_addr.clone();
         tokio::task::spawn(async move {
-            println!("Starting proxy PLAY request with id {id}");
-            let proxy_stream = TcpStream::connect(addr).await;
+            info!("Starting proxy PLAY request with id {id}");
+            let proxy_stream = TcpStream::connect(&*proxy_play).await;
             let mut proxy_stream = match proxy_stream {
                 Ok(s) => s,
                 Err(e) => {
-                    eprintln!("Failed to connect to proxy's PLAY port: {e}");
+                    error!("Failed to connect to proxy's PLAY port: {e}");
                     std::process::exit(1);
                 }
             };
-            println!("Proxy PLAY connected");
-            println!("Connecting to MC server stream");
+            info!("Proxy PLAY connected");
+            info!("Connecting to MC server stream");
             let server_stream = TcpStream::connect(saddr).await;
             let server_stream = match server_stream {
                 Ok(s) => s,
                 Err(e) => {
-                    eprintln!("Failed to connect to server: {e}");
-                    std::process::exit(1);
+                    error!("Failed to connect to MC server: {e}");
+                    error!("Is the MC server up?");
+                    return;
                 }
             };
-            println!("Connected to MC server");
+            info!("Connected to MC server");
             let server_addr = match server_stream.peer_addr() {
                 Ok(a) => a,
                 Err(e) => {
-                    eprintln!("Failed to fetch server's peer addr: {e}");
+                    error!("Failed to fetch server's peer addr: {e}");
                     std::process::exit(1);
                 }
             };
             if let Err(e) = proxy_stream.write_u128(id).await {
-                eprintln!("Failed to send ID to server: {e}");
+                error!("Failed to send ID to server: {e}");
                 std::process::exit(1);
             }
             if let Err(e) = proxy_stream.flush().await {
-                eprintln!("Failed to send ID to server: {e}");
+                error!("Failed to send ID to server: {e}");
                 std::process::exit(1);
             }
             handle_duplex(proxy_stream, server_addr, server_stream).await;
@@ -159,8 +171,8 @@ async fn handle_duplex(
     };
     let mut proxy_stream = proxy_server_stream;
     drop(buf1);
-    println!("Connected {client_addr} to {mc_server_addr}");
-    println!("Beginning proxying...");
+    info!("Connected {client_addr} to {mc_server_addr}");
+    info!("Beginning proxying...");
     let mut buf1 = vec![0u8; 32 * 1024];
     let mut buf2 = vec![0u8; 32 * 1024];
     loop {
@@ -168,7 +180,7 @@ async fn handle_duplex(
             res = proxy_stream.read(&mut buf1) => {
                 match res {
                     Ok(0) => {
-                        println!("Client {client_addr} ended connection with {mc_server_addr}");
+                        info!("Client {client_addr} ended connection with {mc_server_addr}");
                         _ = mc_server_stream.shutdown().await;
                         return;
                     }
@@ -203,7 +215,7 @@ async fn handle_duplex(
             res = mc_server_stream.read(&mut buf2) => {
                 match res {
                     Ok(0) => {
-                        println!("Client {mc_server_addr} ended connection with {client_addr}");
+                        info!("Client {mc_server_addr} ended connection with {client_addr}");
                         _ = mc_server_stream.shutdown().await;
                         return;
                     }
@@ -248,12 +260,12 @@ struct Args {
     /// initially connects to, to request the server to assign it a proxied url
     /// Leave blank to connect to the default port
     #[arg(long, default_value_t = 25564)]
+    proxy_server_connection_port: u16,
     /// The proxy server's PLAY port. This is the port that the host server
     /// connects to once requested by the proxy server, to start proxying the connection.
     /// Leave blank to connect to the default port
-    server_connection_port: u16,
     #[arg(long, default_value_t = 25563)]
-    proxy_server_play: u16,
+    proxy_server_play_port: u16,
     /// The MC server that you want to proxy.
     server_socket_addr: String,
 }
